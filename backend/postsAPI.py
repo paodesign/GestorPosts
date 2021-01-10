fd5f9bb941f521a1a71a2d7824da3f4f5707e4f5
Paola <paomedina_88@hotmail.com>
2021-01-09 21:28:06 -0300

    funciona getById

--- backend/postsAPI.py
+++ backend/postsAPI.py
@@ -36,7 +36,8 @@ class Post():#se crea la clase post
 
 
 class Post_endpoint(Resource): #se crea una clase donde se definiran los endpoint
-    def get(self):# endpoint GET obtiene los post y los envia como objetos en un array "lista_post"
+    @app.route('/api/posts')
+    def getAll():# endpoint GET obtiene los post y los envia como objetos en un array "lista_post"
         conn = db_connect.connect() # se guarda la conexión a base de datos
         query = conn.execute("select * from posts join autores on posts.autor_dni = autores.dni")# se ejecuta la conexión a base de datos y se consulta(obtenga de las tablas posts y la tabla autores,el dni que sea igual en ambas tablas)
         lista_posts = [] #se decalra un array vasió 
@@ -85,60 +86,16 @@ class Post_endpoint(Resource): #se crea una clase donde se definiran los endpoin
         query = conn.execute("update posts set titulo = '{0}', descripcion = '{1}' where id = '{2}'".format(titulo, descripcion, codigo))#se ejecuta la conexión a base de datos y se módifican los datos del post
         return {'status': 'Se a modificado el Post'}
 
-
-
-
-
-class Employees(Resource):
-    def get(self):# se crea una función recibir
-        conn = db_connect.connect() # Conexión a la Base de Datos
-        query = conn.execute("select * from employees")  # Esta línea ejecuta un query y retorna un json como resultado
-        return {'employees': [i[0] for i in query.cursor.fetchall()]}  # Se obtiene la primera columna que es EmployeeId
-
-    def post(self):# se crea una función modificar
-        conn = db_connect.connect()# Conexión a la Base de Datos
-        last_name = request.json['LastName'] #guargamos en esta variable 
-        first_name = request.json['FirstName']
-        title = request.json['Title']
-        reports_to = request.json['ReportsTo']
-        birth_date = request.json['BirthDate']
-        hire_date = request.json['HireDate']
-        address = request.json['Address']
-        city = request.json['City']
-        state = request.json['State']
-        country = request.json['Country']
-        postal_code = request.json['PostalCode']
-        phone = request.json['Phone']
-        fax = request.json['Fax']
-        email = request.json['Email']
-        query = conn.execute("insert into employees values(null,'{0}','{1}','{2}','{3}', \
-                             '{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}', \
-                             '{13}')".format(last_name,first_name,title,
-                                             reports_to, birth_date, hire_date, address,
-                                             city, state, country, postal_code, phone, fax,
-                                             email))
-        return {'status': 'Nuevo empleado insertado'}
-
-
-class Tracks(Resource):
-    def get(self):
+    @app.route('/api/posts/<int:codigo>', methods=['GET'])
+    def getById(codigo):
         conn = db_connect.connect()
-        query = conn.execute("select trackid, name, composer, unitprice from tracks;")
-        result = {'data': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]}
-        return jsonify(result)
+        query = conn.execute("select p.id, p.titulo, p.fecha, p.descripcion, a.nombre, a.dni from posts p join autores a on p.autor_dni = a.dni where id = '{}'".format(codigo))
+        fila = query.cursor.fetchall()[0]
+        post = Post(fila[0], fila[1], fila[2], fila[3], fila[4], fila[5])
+
+        return jsonify(post.__dict__)
 
 
-class EmployeeData(Resource):
-    def get(self, employee_id):
-        conn = db_connect.connect()
-        query = conn.execute("select * from employees where EmployeeId =%d " % int(employee_id))
-        result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
-        return jsonify(result)
-
-
-api.add_resource(Employees, '/employees')  # Route_1
-api.add_resource(Tracks, '/tracks')  # Route_2
-api.add_resource(EmployeeData, '/employees/<employee_id>')  # Route_3
 api.add_resource(Post_endpoint, '/api/posts')
 
 if __name__ == '__main__':

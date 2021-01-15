29ab95e81ea4b5078c95405580cd44bc3bf54894
Paola <paomedina_88@hotmail.com>
2021-01-14 23:25:49 -0300

    funciona crear

--- backend/postsAPI.py
+++ backend/postsAPI.py
@@ -25,14 +25,19 @@ def mensaje():
 api = Api(app)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+class Autor():
+    def __init__(self, nombre, dni, fecha_nac):
+        self.nombre = nombre
+        self.dni = dni
+        self.fecha_nac = fecha_nac.__str__()
+
 class Post():#se crea la clase post
-    def __init__(self, codigo, titulo, fecha, descripcion, autor, dni): #definimos sus atributos
+    def __init__(self, codigo, titulo, fecha, descripcion, autor): #definimos sus atributos
         self.codigo = codigo
         self.titulo = titulo
         self.fecha = fecha.__str__()
         self.descripcion = descripcion
         self.autor = autor
-        self.dni = dni
 
 
 class Post_endpoint(Resource): #se crea una clase donde se definiran los endpoint
@@ -43,7 +48,8 @@ class Post_endpoint(Resource): #se crea una clase donde se definiran los endpoin
         lista_posts = [] #se decalra un array vasió 
 
         for fila in query:
-            post = Post(fila['id'],fila['titulo'], fila['fecha'], fila['descripcion'], fila['nombre'], fila['autor_dni'])
+            autor = Autor(fila['nombre'], fila['autor_dni'],fila['fecha_nac'])
+            post = Post(fila['id'],fila['titulo'], fila['fecha'], fila['descripcion'], autor.__dict__)
             lista_posts.append(post.__dict__) #se agrega post al array
 
         return jsonify(lista_posts)#retorna la respueta en forma de objeto
@@ -82,68 +88,25 @@ class Post_endpoint(Resource): #se crea una clase donde se definiran los endpoin
         codigo = request.args.get("codigo")# se realiza la petición http, que obtenga el código unívoco
         titulo = request.json['titulo']# se  realiza la petición http, se busca la clave para obtener su valor y se lo guarda en una variable
         descripcion = request.json['descripcion']
+        autor = request.json['autor']
 
-        query = conn.execute("update posts set titulo = '{0}', descripcion = '{1}' where id = '{2}'".format(titulo, descripcion, codigo))#se ejecuta la conexión a base de datos y se módifican los datos del post
+        query = conn.execute("select count(*) from autores where dni = {}".format(autor['dni']))# se ejecuta la conexión a base de datos,y se realiza la consulta(queremos saber la cant, de autores con el mismo dni)
+        resultado = query.cursor.fetchone()[0] #se guaraga el resultado obtenido en la consulta
+        if resultado ==  0:# si el resultado es igual a cero se crea un nuevo autor
+            conn.execute("insert into autores values('{0}','{1}','{2}')".format(autor['dni'], autor['nombre'], autor['fecha_nac']))#se ejecuta la conexión, se añade datos a la tabla creando el autor
+
+        query = conn.execute("update posts set titulo = '{0}', descripcion = '{1}', autor_dni = '{2}' where id = '{3}'".format(titulo, descripcion, autor['dni'], codigo)) #se ejecuta la conexión a base de datos y se módifican los datos del post
         return {'status': 'Se a modificado el Post'}
 
-<<<<<<< HEAD
-    def get(self, id):
-        conn = db_connect.connect()
-        codigo = request.args.get("codigo")
-        query = conn.execute("") 
-
-
-
-
-class Employees(Resource):
-    def get(self):# se crea una función recibir
-        conn = db_connect.connect() # Conexión a la Base de Datos
-        query = conn.execute("select * from employees")  # Esta línea ejecuta un query y retorna un json como resultado
-        return {'employees': [i[0] for i in query.cursor.fetchall()]}  # Se obtiene la primera columna que es EmployeeId
-
-    def post(self):# se crea una función modificar
-        conn = db_connect.connect()# Conexión a la Base de Datos
-        last_name = request.json['LastName'] #guargamos en esta variable 
-        first_name = request.json['FirstName']
-        title = request.json['Title']
-        reports_to = request.json['ReportsTo']
-        birth_date = request.json['BirthDate']
-        hire_date = request.json['HireDate']
-        address = request.json['Address']
-        city = request.json['City']
-        state = request.json['State']
-        country = request.json['Country']
-        postal_code = request.json['PostalCode']
-        phone = request.json['Phone']
-        fax = request.json['Fax']
-        email = request.json['Email']
-        query = conn.execute("insert into employees values(null,'{0}','{1}','{2}','{3}', \
-                             '{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}', \
-                             '{13}')".format(last_name,first_name,title,
-                                             reports_to, birth_date, hire_date, address,
-                                             city, state, country, postal_code, phone, fax,
-                                             email))
-        return {'status': 'Nuevo empleado insertado'}
-
-
-class Tracks(Resource):
-    def get(self):
-=======
     @app.route('/api/posts/<int:codigo>', methods=['GET'])
     def getById(codigo):
->>>>>>> fd5f9bb941f521a1a71a2d7824da3f4f5707e4f5
         conn = db_connect.connect()
-        query = conn.execute("select p.id, p.titulo, p.fecha, p.descripcion, a.nombre, a.dni from posts p join autores a on p.autor_dni = a.dni where id = '{}'".format(codigo))
+        query = conn.execute("select p.id, p.titulo, p.fecha, p.descripcion, a.nombre, a.dni, a.fecha_nac from posts p join autores a on p.autor_dni = a.dni where id = '{}'".format(codigo))
         fila = query.cursor.fetchall()[0]
-        post = Post(fila[0], fila[1], fila[2], fila[3], fila[4], fila[5])
+        autor = Autor(fila[4], fila[5],fila[6])
+        post = Post(fila[0], fila[1], fila[2], fila[3], autor.__dict__)
 
         return jsonify(post.__dict__)
-    
-class Autor():
-    def __init__(self, dni, nombre, fecha_nac):
-        self.dni = dni
-        self.nombre = nombre
-        self.fecha_nac = fecha_nac.__str__()
 
 
 class Autor_endpoint(Resource):
@@ -152,7 +115,7 @@ class Autor_endpoint(Resource):
         query = conn.execute("select * from autores")
         lista_autores= []
         for fila in query:
-            autor = Autor(fila['dni'],fila['nombre'], fila['fecha_nac'])
+            autor = Autor(fila['nombre'],fila['dni'], fila['fecha_nac'])
             lista_autores.append(autor.__dict__)
 
         return jsonify(lista_autores)
